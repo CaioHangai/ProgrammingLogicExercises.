8d960da9ab834aa9569c2cc6f46f78e0984d1273
Caio Hangai <kio.cesar@gmail.com>
2022-06-15 23:35:43 +0900

    Iniciando as postagens Exercicios URI Online no GitHub

---
+++ exercicio1002CalculaArea/exercicio1002CalculaArea/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicio1002CalculaArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area, n = 3.14159, raio;
+
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = (n * Math.Pow(raio, 2.0));
+
+            Console.WriteLine("A=" + area.ToString("f4", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
